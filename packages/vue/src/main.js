c47cc3c701be9d26e64bc1230bbf7096932ffa36
Matheus Pessoa <matheus41.dev@gmail.com>
2026-06-05 17:43:36 -0300

    feat(FbInput): add password visibility toggle with oh-vue-icons

--- packages/vue/src/main.js
+++ packages/vue/src/main.js
@@ -1,11 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import FormBlocksInit from './index'
-
+import { OhVueIcon, addIcons } from "oh-vue-icons"
+import { HiEye, HiEyeOff } from "oh-vue-icons/icons/hi"
 import './styles'
 
 const app = createApp(App)
 
 FormBlocksInit.install(app)
+
+addIcons(HiEye, HiEyeOff)
+app.component("v-icon", OhVueIcon)
 
 app.mount('#app')
